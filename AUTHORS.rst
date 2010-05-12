dfe2a97a43a5de6f8342aa925e9107a5c93fdc61
Jannis Leidel <jannis@leidel.info>
2010-05-12 23:34:46 +0800

    Added myself to authors and updated setup file. Bumped to 0.2a2.

--- AUTHORS.rst
+++ AUTHORS.rst
@@ -1,4 +1,5 @@
 Sean Bleier <http://github.com/sebleier>
 Matt Dennewitz <http://github.com/blackbrrr>
+Jannis Leidel <http://github.com/jezdez>
 
 Forked from http://github.com/sebleier/django-redis-cache
--- setup.py
+++ setup.py
@@ -1,26 +1,20 @@
-#!/usr/bin/env python
-
 from distutils.core import setup
-
-version = "0.2a1"
-
-classifiers = [
-    "Programming Language :: Python",
-    "Operating System :: OS Independent",
-    "Topic :: Software Development :: Libraries",
-    "Topic :: Utilities",
-    "Environment :: Web Environment",
-    "Framework :: Django",
-]
 
 setup(
     name = "django-redis-cache",
-    version = version,
     url = "http://github.com/blackbrrr/django-redis-cache/",
     author = "Matt Dennewitz",
     author_email = "mattdennewitz@gmail.com",
+    version = "0.2a2",
     packages = ["redis_cache"],
     description = "Redis Cache Backend for Django",
-    classifiers = classifiers
+    classifiers = [
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+    ],
 )
 
