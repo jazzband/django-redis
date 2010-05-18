612b4c9993fb999a9910c467886e2a0d31a16f8d
Jannis Leidel <jannis@leidel.info>
2010-05-18 21:23:55 +0800

    Bumped version slightly.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     url = "http://github.com/blackbrrr/django-redis-cache/",
     author = "Matt Dennewitz",
     author_email = "mattdennewitz@gmail.com",
-    version = "0.2a4",
+    version = "0.2a5",
     packages = ["redis_cache"],
     description = "Redis Cache Backend for Django",
     classifiers = [
