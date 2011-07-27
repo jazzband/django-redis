19415eaca700af0c69938c605ec0a233bdd43a71
Sean Bleier <sebleier@gmail.com>
2011-07-26 20:56:33 -0700

    Nudged to version 0.8.2.  Addresses handling of integers when using the 'get_many' method.  Fixes issue #20 on github.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     url = "http://github.com/sebleier/django-redis-cache/",
     author = "Sean Bleier",
     author_email = "sebleier@gmail.com",
-    version = "0.8.1",
+    version = "0.8.2",
     packages = ["redis_cache"],
     description = "Redis Cache Backend for Django",
     install_requires=['redis>=2.4.5',],
