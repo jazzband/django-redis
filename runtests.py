72385c17f6ba1c4c4c11811ba6fe783774dd8a6c
Andrei Antoukh <niwi@niwi.be>
2011-08-12 22:02:22 +0200

    Fixed import path on runtests.py.

--- runtests.py
+++ runtests.py
@@ -15,11 +15,11 @@ if not settings.configured:
         ],
         CACHES = {
             'default': {
-                'BACKEND': 'redis_cache.RedisCache',
+                'BACKEND': 'redis_cache.cache.RedisCache',
                 'LOCATION': '127.0.0.1:6379',
                 'OPTIONS': {
                     'DB': 15,
-                    'PASSWORD': 'yadayada',
+                    'PASSWORD': '',
                 },
             },
         },
