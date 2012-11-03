6a69a028ed8d5188586085940183cace5f22aabd
Andrey Antukh <niwi@niwi.be>
2012-11-03 01:07:37 +0100

    Add unicode_literals (__future__) for python3 compatibility.

--- redis_cache/client.py
+++ redis_cache/client.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import
+from __future__ import absolute_import, unicode_literals
 
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.encoding import smart_unicode, smart_str
