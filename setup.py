51c777a29b9b6fcc07459ad894f6c256db8e6c78
Andrey Antukh <niwi@niwi.be>
2012-11-03 14:53:08 +0100

    Update setup.py.

--- setup.py
+++ setup.py
@@ -9,17 +9,14 @@ setup(
     url = "https://github.com/niwibe/django-redis",
     author = "Andrei Antoukh",
     author_email = "niwi@niwi.be",
-    version=':versiontools:redis_cache:',
+    version='3.0',
     packages = [
         "redis_cache",
         "redis_cache.stats"
     ],
     description = description.strip(),
     install_requires=[
-        'redis>=2.4.5',
-    ],
-    setup_requires = [
-        'versiontools >= 1.8',
+        'redis>=2.7.0',
     ],
     zip_safe=False,
     include_package_data = True,
@@ -28,11 +25,18 @@ setup(
     },
     classifiers = [
         "Development Status :: 5 - Production/Stable",
-        "Programming Language :: Python",
         "Operating System :: OS Independent",
-        "Topic :: Software Development :: Libraries",
-        "Topic :: Utilities",
         "Environment :: Web Environment",
         "Framework :: Django",
+        "License :: OSI Approved :: BSD License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
     ],
 )
