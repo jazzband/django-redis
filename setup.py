0d978a468c7823653abd4f60218d89252025863e
Andrey Antukh <niwi@niwi.be>
2013-03-11 23:59:09 +0100

    Increment version to 3.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     url = "https://github.com/niwibe/django-redis",
     author = "Andrei Antoukh",
     author_email = "niwi@niwi.be",
-    version='3.1.6',
+    version='3.2',
     packages = [
         "redis_cache",
         "redis_cache.client",
