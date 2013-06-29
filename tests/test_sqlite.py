9fb76b09303d842aacd3fb5be33fd05baefb4069
Andrey Antukh <niwi@niwi.be>
2013-06-29 11:45:59 +0200

    Minor change on testing settings.

--- tests/test_sqlite.py
+++ tests/test_sqlite.py
@@ -27,7 +27,10 @@ STATICFILES_DIRS = ()
 CACHES = {
     'default': {
         'BACKEND': 'redis_cache.cache.RedisCache',
-        'LOCATION': '127.0.0.1:6379:1,127.0.0.1:6379:1',
+        'LOCATION': [
+            '127.0.0.1:6379:1',
+            '127.0.0.1:6379:1',
+        ],
         'OPTIONS': {
             'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
         }
