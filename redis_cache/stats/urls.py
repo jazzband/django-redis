6375aba1b7f69607ce9d0f10d7826c192419966d
Alexander <a.a.lebedev@gmail.com>
2013-07-05 10:30:20 +0600

    Update urls.py
    Fixed Django 1.5 DeprecationWarning

--- redis_cache/stats/urls.py
+++ redis_cache/stats/urls.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls import *
+except ImportError: # django < 1.4
+    from django.conf.urls.defaults import *
+
 from .views import RedisStatsView
 
 urlpatterns = patterns('',
