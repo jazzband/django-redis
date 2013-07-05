61811e78b3a2340012916a235984d43ac0057e19
Alexander <a.a.lebedev@gmail.com>
2013-07-05 10:46:45 +0600

    Update urls.py
    Fixed a typo

--- redis_cache/stats/urls.py
+++ redis_cache/stats/urls.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 try:
-    from django.conf.urls import patterns, url
+    from django.conf.urls import *
 except ImportError: # django < 1.4
-    from django.conf.urls.defaults import patterns, url
+    from django.conf.urls.defaults import *
 
 from .views import RedisStatsView
 
