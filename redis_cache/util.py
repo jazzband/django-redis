00e1d0bb098c0b8d5c53e23db04bd23c1b72654b
Bertrand Bordage <bordage.bertrand@gmail.com>
2013-11-26 11:05:10 +0100

    Removes an unused util.

--- redis_cache/util.py
+++ redis_cache/util.py
@@ -52,19 +52,6 @@ class CacheKey(object):
         return key
 
 
-class Singleton(type):
-    """ Singleton metaclass. """
-
-    def __init__(cls, name, bases, dct):
-        cls.__instance = None
-        type.__init__(cls, name, bases, dct)
-
-    def __call__(cls, *args, **kw):
-        if cls.__instance is None:
-            cls.__instance = type.__call__(cls, *args,**kw)
-        return cls.__instance
-
-
 def load_class(path):
     """
     Load class from path.
