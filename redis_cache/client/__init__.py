bcaf887ccad40adf2cb09627c12f2a3e1b4b006d
Andrey Antukh <niwi@niwi.be>
2014-02-10 20:10:27 +0100

    Disable Sentinel client with redis-py < 2.9

--- redis_cache/client/__init__.py
+++ redis_cache/client/__init__.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 
+import warnings
+
 from .default import DefaultClient
 from .sharded import ShardClient
 from .herd import HerdClient
 from .experimental import SimpleFailoverClient
-from .sentinel import SentinelClient
+
 
 __all__ = ['DefaultClient', 'ShardClient',
-           'HerdClient', 'SimpleFailoverClient',
-           'SentinelClient']
+           'HerdClient', 'SimpleFailoverClient',]
+
+try:
+    from .sentinel import SentinelClient
+    __all__.append("SentinelClient")
+except ImportError:
+    warnings.warn("sentinel client is unsuported with redis-py<2.9",
+                  RuntimeWarning)
+
