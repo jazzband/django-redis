d6cd36320467f3d265536847a6a0c9c1cf38a2ad
Andrey Antukh <niwi@niwi.be>
2014-03-07 21:20:45 +0100

    Add copy static files to build-docs script.

--- build-docs.sh
+++ build-docs.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 (cd doc; make)
 cp -vr doc/index.html /tmp/index.html;
+cp -vr doc/static /tmp/static
 git checkout gh-pages;
 rm -rf *
 mv -fv /tmp/index.html .
+mv -fv /tmp/static .
 git add --all index.html
 git commit -a -m "Update doc"
