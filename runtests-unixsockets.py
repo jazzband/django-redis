74778ed99cfbe89d889acdb442e24e1c496e0ab4
Andrey Antukh <niwi@niwi.be>
2014-03-23 22:19:10 +0100

    Add scripts for manual testing over unix sockets.

---
+++ runtests-unixsockets.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_sqlite_usock")
+sys.path.insert(0, 'tests')
+
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+    args = sys.argv
+    args.insert(1, "test")
+
+    execute_from_command_line(args)
---
+++ tests/test_sqlite_usock.py
@@ -0,0 +1,59 @@
+# This is an example test settings file for use with the Django test suite.
+#
+# The 'sqlite3' backend requires only the ENGINE setting (an in-
+# memory database will be used). All other backends will require a
+# NAME and potentially authentication information. See the
+# following section in the docs for more information:
+#
+# https://docs.djangoproject.com/en/dev/internals/contributing/writing-code/unit-tests/
+#
+# The different databases that Django supports behave differently in certain
+# situations, so it is recommended to run the test suite against as many
+# database backends as possible.  You may want to create a separate settings
+# file for each of the backends you test against.
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3'
+    },
+}
+
+SECRET_KEY = "django_tests_secret_key"
+TIME_ZONE = 'America/Chicago'
+LANGUAGE_CODE = 'en-us'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+STATICFILES_DIRS = ()
+
+CACHES = {
+    'default': {
+        'BACKEND': 'redis_cache.cache.RedisCache',
+        'LOCATION': [
+            'unix:/tmp/redis.sock:1',
+            'unix:/tmp/redis.sock:1',
+        ],
+        'OPTIONS': {
+            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
+        }
+    },
+    'doesnotexist': {
+        'BACKEND': 'redis_cache.cache.RedisCache',
+        'LOCATION': '127.0.0.1:56379:1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
+        }
+    },
+    'sample': {
+        'BACKEND': 'redis_cache.cache.RedisCache',
+        'LOCATION': '127.0.0.1:6379:1,127.0.0.1:6379:1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
+        }
+    },
+}
+
+TEST_RUNNER = 'django.test.simple.DjangoTestSuiteRunner'
+
+INSTALLED_APPS = (
+    'redis_backend_testapp',
+    'hashring_test',
+)
