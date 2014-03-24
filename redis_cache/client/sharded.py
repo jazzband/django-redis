150e54d969cd72097032213b152a6e05f78e51dd
Andrey Antukh <niwi@niwi.be>
2014-03-24 21:28:21 +0100

    Fix compatibility with sharded client.

--- redis_cache/client/sharded.py
+++ redis_cache/client/sharded.py
@@ -35,8 +35,7 @@ class ShardClient(DefaultClient):
         connection_dict = {}
         for name in self._server:
             host, port, db = self.parse_connection_string(name)
-            connection_dict[name] = self._connect(host, port, db)
-
+            connection_dict[name] = self.connection_factory.connect(host, port, db)
         return connection_dict
 
     def get_server_name(self, _key):
