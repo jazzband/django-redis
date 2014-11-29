53f9b99482655efb5119729667f3a755b98c34db
Andrey Antukh <niwi@niwi.be>
2014-11-29 18:59:45 +0100

    Remove sentinel client from codebase.
    Pending move it to separated package.

--- django_redis/client/__init__.py
+++ django_redis/client/__init__.py
@@ -2,11 +2,9 @@ from .default import DefaultClient
 from .sharded import ShardClient
 from .herd import HerdClient
 from .dummy import RedisDummyCache
-from .sentinel import SentinelClient
 
 
 __all__ = ["DefaultClient",
            "ShardClient",
            "HerdClient",
-           "RedisDummyCache",
-           "SentinelClient"]
+           "RedisDummyCache"]
