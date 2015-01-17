076386e902a9956b20667dc944fdcf198c2cc691
Andrey Antukh <niwi@niwi.be>
2015-01-17 12:18:00 +0100

    Update old build docs script.

--- build-docs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-(cd doc; make)
-cp -vr doc/index.html /tmp/index.html;
-git checkout gh-pages;
-rm -rf index.html
-rm -rf static
-mv -fv /tmp/index.html .
-git add --all index.html
-git commit -a -m "Update doc"
