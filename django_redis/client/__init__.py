e0d6b2e94d54ce5a658b7acfcff1f80322ccbb0f
Chris Heisel <cheisel@heisels-mbp.kabbage.com>
2015-04-15 21:09:22 -0400

    Add removed sentinel

--- django_redis/client/__init__.py
+++ django_redis/client/__init__.py
@@ -2,9 +2,10 @@ from .default import DefaultClient
 from .sharded import ShardClient
 from .herd import HerdClient
 from .dummy import RedisDummyCache
-
+from .sentinel import SentinelClient
 
 __all__ = ["DefaultClient",
            "ShardClient",
            "HerdClient",
-           "RedisDummyCache"]
+           "RedisDummyCache",
+           "SentinelClient"]
