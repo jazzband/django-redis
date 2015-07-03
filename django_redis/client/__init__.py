3436734bc5770d1a4a6c3c37029218765454bae9
Andrey Antukh <niwi@niwi.nz>
2015-07-03 21:38:27 +0300

    Remove dummy.py module.

--- django_redis/client/__init__.py
+++ django_redis/client/__init__.py
@@ -1,10 +1,8 @@
 from .default import DefaultClient
 from .sharded import ShardClient
 from .herd import HerdClient
-from .dummy import RedisDummyCache
 
 
 __all__ = ["DefaultClient",
            "ShardClient",
-           "HerdClient",
-           "RedisDummyCache"]
+           "HerdClient"]
