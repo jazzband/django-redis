40f9e7bf44bbef3ab4a31103c3e64a7e1da33edf
Andrey Antukh <niwi@niwi.nz>
2015-10-31 09:29:15 +0200

    Add python 3.5 to the classifiers list.

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Topic :: Software Development :: Libraries",
         "Topic :: Utilities",
     ],
