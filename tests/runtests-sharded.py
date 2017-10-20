0b2c8079178a575393bc4fe18731cb6a1a936993
Jon Dufresne <jon.dufresne@gmail.com>
2017-10-20 14:43:45 -0700

    Fix tests and tox configuration to run successfully
    Previously, tox was executing 0 tests.
    
    Reorganize the envlist in terms of Django versions. Much easier to keep
    organized and (when it is time) drop outdated version. The tox and
    Travis matrix now match.
    
    Required adding missing dependency to tox configuration.
    
    Add missing supported Python version to trove classifiers. Python 3.3 is
    still being tested by tox and Travis CI.

--- tests/runtests-sharded.py
+++ tests/runtests-sharded.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-sys.path.insert(0, '..')
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_sqlite_sharding")
 
 if __name__ == "__main__":
