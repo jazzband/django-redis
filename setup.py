dd3e78baaa880d93fda3c0fecf484683bab0c3e8
Jon Dufresne <jon.dufresne@gmail.com>
2017-12-03 09:43:30 -0800

    Add testing and support for Django 2.0
    No actual code changes were required.

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         "Framework :: Django :: 1.8",
         "Framework :: Django :: 1.10",
         "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
