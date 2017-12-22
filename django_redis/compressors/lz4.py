26251a2dc48ce8e96aa6aea1f35856ecf50e4e79
excentro <excentro@gmail.com>
2017-12-22 15:39:14 +0300

    working with latest lz4

--- django_redis/compressors/lz4.py
+++ django_redis/compressors/lz4.py
@@ -11,16 +11,14 @@ from .base import BaseCompressor
 
 class Lz4Compressor(BaseCompressor):
     min_length = 15
-    preset = 8
 
     def compress(self, value):
         if len(value) > self.min_length:
-            return _compress(
-                source=value, compression_level=self.preset)
+            return _compress(value, return_bytearray=False, store_size=False)
         return value
 
     def decompress(self, value):
         try:
-            return _decompress(value)
+            return _decompress(value, return_bytearray=False)
         except Exception as e:
             raise CompressorError(e)
