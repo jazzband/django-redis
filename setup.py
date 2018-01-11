8e48b3342bbd3c43793b23f364be70ef3c601798
Jon Dufresne <jon.dufresne@gmail.com>
2018-01-10 20:36:30 -0800

    Pass python_requires argument to setuptools
    Helps pip decide what version of the library to install.
    
    https://packaging.python.org/tutorials/distributing-packages/#python-requires
    
    > If your project only runs on certain Python versions, setting the
    > python_requires argument to the appropriate PEP 440 version specifier
    > string will prevent pip from installing the project on other Python
    > versions.
    
    https://setuptools.readthedocs.io/en/latest/setuptools.html#new-and-changed-setup-keywords
    
    > python_requires
    >
    > A string corresponding to a version specifier (as defined in PEP 440)
    > for the Python version, used to specify the Requires-Python defined in
    > PEP 345.

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
         "django_redis.compressors"
     ],
     description=description.strip(),
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     install_requires=[
         "Django>=1.11",
         "redis>=2.10.0",
