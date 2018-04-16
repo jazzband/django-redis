f6a196aafd48158db1428cf74067b5e4d54c5c0b
Sven Auhagen <Sven.Auhagen@voleatech.de>
2018-04-16 09:08:57 +0200

    Add dill serializer

---
+++ django_redis/serializers/dill.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import dill
+
+from __future__ import absolute_import, unicode_literals
+
+from django.core.exceptions import ImproperlyConfigured
+
+from .base import BaseSerializer
+
+class DillSerializer(BaseSerializer):
+    def __init__(self, options):
+        self._pickle_version = -1
+        self.setup_pickle_version(options)
+
+    def setup_pickle_version(self, options):
+        if "PICKLE_VERSION" in options:
+            try:
+                self._pickle_version = int(options["PICKLE_VERSION"])
+            except (ValueError, TypeError):
+                raise ImproperlyConfigured("PICKLE_VERSION value must be an integer")
+
+    def dumps(self, value):
+        return dill.dumps(value, self._pickle_version)
+
+    def loads(self, value):
+        return dill.loads(value)
