3bdb748d832ef10c2c200ccb0b90ba67a2d3d798
Kiriushin Vasilii <theoldmop@yandex.ru>
2018-07-20 03:37:52 +1000

    fix get() settings check

--- django_redis/cache.py
+++ django_redis/cache.py
@@ -80,7 +80,7 @@ class RedisCache(BaseCache):
             return self.client.get(key, default=default, version=version,
                                    client=client)
         except ConnectionInterrupted as e:
-            if DJANGO_REDIS_IGNORE_EXCEPTIONS or self._ignore_exceptions:
+            if self._ignore_exceptions:
                 if DJANGO_REDIS_LOG_IGNORED_EXCEPTIONS:
                     logger.error(str(e))
                 return default
