acb83ba2480dbed7844c16b74f15ac6cec1b7f8c
Jon Dufresne <jon.dufresne@gmail.com>
2019-12-15 11:14:13 -0800

    Fix msgpack deprecation warning:
        DeprecationWarning: encoding is deprecated, Use raw=False instead.

--- django_redis/serializers/msgpack.py
+++ django_redis/serializers/msgpack.py
@@ -8,4 +8,4 @@ class MSGPackSerializer(BaseSerializer):
         return msgpack.dumps(value)
 
     def loads(self, value):
-        return msgpack.loads(value, encoding="utf-8")
+        return msgpack.loads(value, raw=False)
