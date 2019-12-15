91e3be6c603c241b64b5fb2ad778df56e55acdd4
Jon Dufresne <jon.dufresne@gmail.com>
2019-12-15 14:01:44 -0800

    Allow overriding the encoder class for the JSON serializer

--- django_redis/serializers/json.py
+++ django_redis/serializers/json.py
@@ -6,8 +6,10 @@ from .base import BaseSerializer
 
 
 class JSONSerializer(BaseSerializer):
+    encoder_class = DjangoJSONEncoder
+
     def dumps(self, value):
-        return json.dumps(value, cls=DjangoJSONEncoder).encode()
+        return json.dumps(value, cls=self.encoder_class).encode()
 
     def loads(self, value):
         return json.loads(value.decode())
