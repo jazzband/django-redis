0791b0271dcb10b3bc4c81b0ea395fb4d47efcf6
Terence D. Honles <terence@honles.com>
2021-04-05 15:40:29 -0700

    deduplicate test runners and organize test settings

--- tests/shell.py
+++ tests/shell.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python
 import os
 import sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_sqlite")
-sys.path.insert(0, "..")
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.sqlite")
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
